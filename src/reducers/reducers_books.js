3fc4b29bc7e755e08028834d2564eeb26bcae974
Harmeet Singh <harmeet@clausehound.com>
2016-12-05 15:18:10 -0500

    s4l39

---
+++ src/reducers/reducers_books.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Harmeet Singh on 12/5/2016.
+ */
+
+export default function() {
+  return [
+    {title: 'Javascript: The Good Parts'},
+    {title: 'Harry Potter & The Half Blood Prince'},
+    {title: 'The Dark Tower'},
+    {title: 'Eloquent Ruby'},
+  ];
+}
