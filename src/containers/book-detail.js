d961ab93ab4436d8ea1285bda102cbff746ee1e0
Harmeet Singh <harmeet@clausehound.com>
2016-12-12 15:37:53 -0500

    section 4 done

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -9,6 +9,7 @@ class BookDetail extends Component {
     if(!this.props.book){
       return <div>Select a book to get started!</div>
     }
+
     return (
       <div>
         <h3>Details for:</h3>
